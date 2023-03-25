a3ca12758eaca09607ee98997a67e3c54e489037
joaopespindola <joaopedro.espindola@outlook.com>
2023-03-25 15:16:45 -0300

    pequenas alterações

--- Curso em video/ex002.py
+++
@@ -1,2 +0,0 @@
-nome=input('Qual o seu nome?')
-print('Olá, prazer {} em conhecê-lo !'.format(nome))
--- Curso em video/ex006.py
+++
@@ -1,2 +0,0 @@
-x=int(input('Digite um valor inteiro: '))
-print('O dobro de {} é igual a {}\nO triplo de {} é igual a {} \nA raiz quadrada de {} é igual a {:.2f}' .format(x, (x*2), x, (x*3), x, (x**(1/2))))
